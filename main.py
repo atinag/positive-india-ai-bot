dad13a56e82ce280098407b566497e389976ede8
Atin Agarwal <atinagarwal@live.com>
2025-04-15 16:07:11 +0530

    cleaning code

--- main.py
+++ main.py
@@ -7,25 +7,14 @@ from openai import AzureOpenAI
 
 
 # Azure OpenAI configuration
-# openai.api_type = "azure"
-# openai.api_base = "https://azureopenaipoistiveindiabotinstance.openai.azure.com/"
-# openai.api_version = "2024-12-01-preview"
-# openai.api_key = os.getenv("OPENAI_API_KEY")
-
 
 client = AzureOpenAI(
     api_key=os.getenv("OPENAI_API_KEY"),
     api_version="2023-12-01-preview",
     azure_endpoint="https://azureopenaipoistiveindiabotinstance.openai.azure.com/",
 )
-
 AZURE_DEPLOYMENT_NAME = "gpt-35-turbo"  
 
-BEARER_TOKEN = os.getenv("TWITTER_BEARER_TOKEN")
-headers = {
-    "Authorization": f"Bearer {BEARER_TOKEN}",
-    "Content-Type": "application/json"
-}
 
 # API Keys from GitHub Secrets
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
@@ -37,8 +26,6 @@ access_token = os.getenv("TWITTER_ACCESS_TOKEN")
 access_token_secret = os.getenv("TWITTER_ACCESS_SECRET")
 
 # Set up tweepy client
-# auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-# api = tweepy.API(auth)
 tweepyclient = tweepy.Client(
     consumer_key = consumer_key, consumer_secret = consumer_secret,
     access_token=access_token, access_token_secret=access_token_secret)
@@ -56,16 +43,7 @@ def get_positive_news():
 # Function to summarize the news using OpenAI
 def summarize_news(news):
     prompt = f"Summarize the following positive news about India:\n\n{news}\n\nSummary:"
-    # response = openai.ChatCompletion.create(
-    #     engine="gpt-35-turbo",
-    #     messages=[
-    #         {"role": "system", "content": "You are an assistant that summarizes positive news from India."},
-    #         {"role": "user", "content": prompt}
-    #     ],
-    #     temperature=0.7,
-    #     max_tokens=100,
-    # )
-    # return response["choices"][0]["message"]["content"].strip()
+    
     response = client.chat.completions.create(
         model=AZURE_DEPLOYMENT_NAME,  # This should be your deployment name
         messages=[
@@ -81,18 +59,14 @@ def summarize_news(news):
 
 # Function to post the summary to Twitter
 def post_to_twitter(summary):
-    # api.update_status(summary)
+   
     print(f"Summary is:\n{summary}")
     print(f"Type of summary: {type(summary)}")
 
     if len(summary) > 280:
         summary = summary[:277] + "..."
-
     # Now tweet
     response = tweepyclient.create_tweet(text=summary)
-
-    # response = tweepyclient.create_tweet(
-    # text=summary.)
 
 
 # Main function
